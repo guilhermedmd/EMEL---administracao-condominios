c6f05d72f3c29d94c0fc2fa5a3113cc73618c0c5
Guilherme Domingues <guilhermegdmd@gmail.com>
2026-04-12 23:01:42 -0300

    Ajustes na inicial page

--- lib/pages/inicial_page.dart
+++ lib/pages/inicial_page.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:emel/pages/login_page.dart';
+import 'package:emel/pages/pagamentos_page.dart';
 
-//Stateless pois não muda
+// Stateless pois a tela é estática
 class InicialPage extends StatelessWidget {
   const InicialPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // [RC5] largura e altura para responsividade
+    final double larguraTela = MediaQuery.of(context).size.width;
+    final double alturaTela = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: const Color(0xFFE6EFEA),
-      // [RC7] Uso de AppBar
+      // [RC7] Uso de AppBar 
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         toolbarHeight: 40,
       ),
-      //
       body: SafeArea(
         child: Padding(
-          //para que não batam nas bordas
           padding: const EdgeInsets.symmetric(horizontal: 24),
-          // [RC5] SingleChildScrollView para ser responsivo 
+          // [RC5] - Scrollview conteúdo acessível no paisagem
           child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 20),
-                /// LOGO
-                const Icon(Icons.home_work, size: 70, color: Colors.black87),
+                SizedBox(height: alturaTela * 0.05),
+                /// LOGO RESPONSIVA [RC5]
+                Icon(
+                  Icons.home_work, 
+                  size: larguraTela * 0.2, // ícone se ajusta ao tamanho da tela
+                  color: Colors.black87
+                ),
+                
                 const SizedBox(height: 10),
                 const Text(
                   'EMEL',
@@ -53,10 +60,11 @@ class InicialPage extends StatelessWidget {
                 const SizedBox(height: 24),
 
                 /// BOTÃO MORADOR [RC2] e [RC7]
-             SizedBox(
+                SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
                     onPressed: () {
+                      // Navegação para a tela de login
                       Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
                     },
                     style: ElevatedButton.styleFrom(
@@ -70,11 +78,13 @@ class InicialPage extends StatelessWidget {
 
                 const SizedBox(height: 16),
 
-                /// BOTÃO VISITANTE [RC2]
+                /// BOTÃO VISITANTE 
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => TelaPagamentos()));
+                    },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xFFD4E2DB),
                       padding: const EdgeInsets.symmetric(vertical: 16),
@@ -84,7 +94,7 @@ class InicialPage extends StatelessWidget {
                   ),
                 ),
                 
-                const SizedBox(height: 20),
+                const SizedBox(height: 40), // para evitar que o botão encoste na borda 
               ],
             ),
           ),
@@ -92,5 +102,4 @@ class InicialPage extends StatelessWidget {
       ),
     );
   }
-
 }
