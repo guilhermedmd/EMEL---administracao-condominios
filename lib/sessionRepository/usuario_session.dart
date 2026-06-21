c23093f5d42b9f8c862bca092215f0777fdeff2a
Ian Souza <ian.souza@aluno.ifsp.edu.br>
2026-06-21 12:49:26 -0300

    Teste do POST - tela 6
    Teste do endpoint POST da tela 6 do documento

--- lib/sessionRepository/usuario_session.dart
+++ lib/sessionRepository/usuario_session.dart
@@ -2,55 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:hive_ce/hive_ce.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class UsuarioSession extends ChangeNotifier{
+class UsuarioSession extends ChangeNotifier {
+
   final usuarioBox = Hive.box("usuario");
 
-  Future<void> salvarMorador(PostgrestMap morador)async{
+  Future<void> salvarMorador(PostgrestMap morador) async {
     await usuarioBox.put("user", morador);
     notifyListeners();
   }
 
-  Future<void> salvarVisitante(PostgrestMap visitante)async{
+  Future<void> salvarVisitante(PostgrestMap visitante) async {
     await usuarioBox.put("user", visitante);
     notifyListeners();
   }
 
-  String get nomeUsuario{
-  final usuario =
-      usuarioBox.get("user");
-  if (usuario == null) {
-    return "";
-  }
+  String get nomeUsuario {
+    final usuario = usuarioBox.get("user");
+
+    if (usuario == null) {
+      return "";
+    }
 
     return usuario["nome"];
   }
-  int get idMorador{
-    final usuario =
-        usuarioBox.get("user");
+
+  int get idMorador {
+    final usuario = usuarioBox.get("user");
+
     if (usuario == null) {
-      throw Exception("Não há id do morador");
+      return 0;
     }
 
     return usuario["id_morador"];
   }
 
-  Future<void> logoutUserSession()async{
+  Future<void> logoutUserSession() async {
     await usuarioBox.clear();
-  int get idMorador {
-  final usuario = usuarioBox.get("user");
-
-  if(usuario == null){
-    return 0;
-  }
-    return usuario["id_morador"];
+    notifyListeners();
   }
 
-  void logout(){
+  void logout() {
     usuarioBox.clear();
     notifyListeners();
   }
 
+}
   // prováveis gets para o endereço, 
   //no caso a moradia do usuario deverão ser feitos
-}
-}
